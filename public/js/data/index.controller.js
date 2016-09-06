de0bda2c7c4226e60b9c3464d4186e930411663b
pamelaolney <pjolney12@gmail.com>
2016-09-06 18:46:42 -0400

    addclass remove class function now working with check function

--- public/js/data/index.controller.js
+++ public/js/data/index.controller.js
@@ -1,29 +1,30 @@
 "use strict";
 
-  (function(){
-    angular
-    .module("data")
-    .controller("DataIndexController", [
-      "LetterFactory",
-      DataIndexControllerFunction
-    ]);
+(function(){
+  angular
+  .module("data")
+  .controller("DataIndexController", [
+    "LetterFactory",
+    DataIndexControllerFunction
+  ]);
 
-    function DataIndexControllerFunction(LetterFactory){
-      this.data = LetterFactory.query();
-      this.shuffle = function(o){
-        for(var j, x, i = this.data.length; i; j = Math.floor(Math.random() * i),
-        x = this.data[--i], this.data[i] = this.data[j], this.data[j] = x);
-        return this.data;
-      };
+  function DataIndexControllerFunction(LetterFactory){
+    this.data = LetterFactory.query();
+    this.shuffle = function(o){
+      for(var j, x, i = this.data.length; i; j = Math.floor(Math.random() * i),
+      x = this.data[--i], this.data[i] = this.data[j], this.data[j] = x);
+      return this.data;
+    };
 
 
-    this.check = function(letter){
-        if( letter === "F" || letter === "O" || letter === "C" || letter === "U" || letter === "S"){
-          alert("You did it!!!");
-        }
+    this.check = function(data, letter){
+      data.circle = true;
+      if( data.letter === "F" || data.letter === "O" || data.letter === "C" || data.letter === "U" || data.letter === "S"){
+        alert("You did it!!!");
+      }
     }
 
 
   };
 
-  }());
+}());
